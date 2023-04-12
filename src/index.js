70204d7f0c2bdc4cca9a05649f0e4efc3c09c6bd
“Nicolas <nicolas.e.bron@gmail.com>
2023-04-11 20:27:26 -0400

    part 6 completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import * as module from './initial-page-load.js'
+import { initialPageLoad } from './initial-page-load.js';
+import { menu } from './menu.js';
+import { contact } from './contact.js'; 
 
 
-console.log("This is my console.log statement from /src/index.js");
-module.initialPageLoad();
+initialPageLoad();
+
+// Tab switching module
+let tabSwitchModule = (function() {
+    
+    const homeTab = document.querySelector(".home");
+    homeTab.addEventListener("click", initialPageLoad);
+
+    const menuTab = document.querySelector(".menu");
+    menuTab.addEventListener("click", menu);
+
+    const contactTab = document.querySelector(".contact");
+    contactTab.addEventListener("click", contact);
+
+})();
